f863b5e5e0e9e2db7cca0e6e9a9e6ad952ad8f66
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-29 23:46:36 -0500

    Changes

--- DevExam/Controllers/CustomerController.cs
+++ DevExam/Controllers/CustomerController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DevExam.Controllers
 {
-    public class CustomerController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
     {
         private readonly ICustomerService _context;
 
@@ -11,9 +13,10 @@ namespace DevExam.Controllers
             _context = context;
         }
 
-        public ActionResult<List<string>> GetPersonalData(double amount) {
+        [HttpGet]
+        public ActionResult<List<string>> GetPersonalData([FromRoute]double amount) {
             var customerPersonalData = _context.GetCustomerPersonalDataList(amount);
-            return customerPersonalData == null ? NotFound()  : Ok(_context.GetCustomerPersonalDataList(amount));
+            return customerPersonalData.Count.Equals(0) ? NotFound()  : Ok(_context.GetCustomerPersonalDataList(amount));
         }
     }
 }
